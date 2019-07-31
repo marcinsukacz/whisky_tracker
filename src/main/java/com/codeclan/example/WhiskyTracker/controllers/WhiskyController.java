8ac548485b49666f4fabd31c43da6545920160ba
marcinsukacz <marcin.sukacz@gmail.com>
2019-08-01 00:36:13 +0100

    mvp done

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,35 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/whiskies")
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping
+    public List<Whisky> getAllWhiskies(){
+        return whiskyRepository.findAll();
+    }
+
+    @GetMapping(value = "/year/{year}")
+    public List<Whisky> findWhiskyByYear(@PathVariable int year){
+        return whiskyRepository.findWhiskyByYear(year);
+    }
+
+    @GetMapping(value = "/distillery/{id}/age/{age}")
+    public List<Whisky> findWhiskyByDistilleryAndAge(@PathVariable Long id, @PathVariable int age){
+        return whiskyRepository.findWhiskyByDistilleryIdAndAge(id, age);
+    }
+
+}
---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepository.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public interface DistilleryRepository extends JpaRepository<Distillery, Long>{
+
+
+    List<Distillery> findDistilleriesByRegion(String region);
+}
